ba255b8fdf0a1ca07fef9a059899962e7dd5e01a
kalbetre-alx <kalbetre@icloud.com>
2022-11-14 16:34:22 +0300

    and include and func def

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,8 @@
 #ifndef BINARY_TREE_H
 #define BINARY_TREE_H
 
+#include <stdlib.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -23,5 +25,6 @@ typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
 void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 #endif
