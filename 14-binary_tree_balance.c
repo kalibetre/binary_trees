555fde211b213a7d8727f6270a6811f2e0a41a41
kalbetre-alx <kalbetre@icloud.com>
2022-11-15 11:37:02 +0300

    cast the return value

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,31 +8,31 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	size_t l_height, r_height;
+	int l_height, r_height;
 
 	if (tree == NULL)
 		return (0);
 
-	l_height = tree->left == NULL ? -1 : binary_tree_height_mod(tree->left);
-	r_height = tree->right == NULL ? -1 : binary_tree_height_mod(tree->right);
+	l_height = tree->left == NULL ? -1 : (int)_binary_tree_height(tree->left);
+	r_height = tree->right == NULL ? -1 : (int)_binary_tree_height(tree->right);
 	return (l_height - r_height);
 }
 
 /**
- * binary_tree_height_mod - returns the height of a binary tree
+ * _binary_tree_height - returns the height of a binary tree
  * @tree: the binary tree
  *
  * Return: the height of the binary tree
  */
-size_t binary_tree_height_mod(const binary_tree_t *tree)
+size_t _binary_tree_height(const binary_tree_t *tree)
 {
 	size_t l_height, r_height, height;
 
 	if (tree == NULL)
 		return (0);
 
-	l_height = binary_tree_height_mod(tree->left);
-	r_height = binary_tree_height_mod(tree->right);
+	l_height = _binary_tree_height(tree->left);
+	r_height = _binary_tree_height(tree->right);
 	height = l_height > r_height ? l_height : r_height;
 
 	if (tree->left != NULL || tree->right != NULL)
