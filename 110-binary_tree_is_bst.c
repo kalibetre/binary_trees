b177eded91e83882d7dd98c517f99fff1119fb06
kalbetre-alx <kalbetre@icloud.com>
2022-11-16 20:17:02 +0300

    add BST check

---
+++ 110-binary_tree_is_bst.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+
+/**
+ * _binary_tree_inorder - traverses a tree in in-order traversal
+ *
+ * @tree: pointer to the root node of a tree
+ * @func: pointer to the function to call for each node
+ */
+int _binary_tree_inorder(const binary_tree_t *tree, const binary_tree_t *prev)
+{
+	int check = 0;
+	if (tree == NULL)
+		return (0);
+
+	check = _binary_tree_inorder(tree->left, tree);
+	if (prev != NULL && (check < 0 || prev->n >= tree->n))
+		return (-1);
+	return (_binary_tree_inorder(tree->right, tree));
+}
+
+/**
+ * binary_tree_is_bst - checks if a binary tree is Binary Search Tree
+ *
+ * @tree: the root node of a binary tree
+ * Return: 1 if it is a BST, otherwise 0
+ */
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (_binary_tree_inorder(tree, NULL) < 0)
+		return (0);
+	return (1);
+}
