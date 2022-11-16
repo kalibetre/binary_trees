1b4f11f45965afd0f1875030eae2dc6f2114d9de
kalbetre-alx <kalbetre@icloud.com>
2022-11-16 23:11:20 +0300

    add bst search function

---
+++ 113-bst_search.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * bst_search - search for a value in binary search tree
+ *
+ * @tree: the root node to a bst
+ * @value: value to be searched
+ * @return: node containing the value or NULL
+ */
+bst_t *bst_search(const bst_t *tree, int value)
+{
+	if (tree == NULL)
+		return (NULL);
+
+	if (value == tree->n)
+		return ((bst_t *)tree);
+
+	if (value < tree->n)
+		return (bst_search(tree->left, value));
+	else
+		return (bst_search(tree->right, value));
+}
