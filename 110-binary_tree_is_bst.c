0a51030d247374cd285f148604122c43898b600e
kalbetre-alx <kalbetre@icloud.com>
2022-11-16 21:35:25 +0300

    fix logic for saving visited node

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -4,23 +4,20 @@
  * _binary_tree_inorder - traverses a tree in in-order traversal
  *
  * @tree: pointer to the root node of a tree
- * @prev: pointer to the prev visited node
- * Return: 0 if it is bts otherwise -1
+ * @prev: pointer to the last visited node
+ * Return: 1 if it is bts otherwise 0
  */
-int _binary_tree_inorder(const binary_tree_t *tree, const binary_tree_t *prev)
+int _binary_tree_inorder(const binary_tree_t *tree, binary_tree_t **prev)
 {
 	int check = 0;
 
 	if (tree == NULL)
-		return (0);
-
-	if (tree->left != NULL && tree->n <= tree->left->n)
-		return (-1);
+		return (1);
 
 	check = _binary_tree_inorder(tree->left, prev);
-	if (check < 0 || (prev != NULL && prev->n >= tree->n))
-		return (-1);
-	prev = tree;
+	if (check == 0 || (*prev != NULL && (*prev)->n >= tree->n))
+		return (0);
+	*prev = (binary_tree_t *)tree;
 	return (_binary_tree_inorder(tree->right, prev));
 }
 
@@ -32,10 +29,10 @@ int _binary_tree_inorder(const binary_tree_t *tree, const binary_tree_t *prev)
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
+	binary_tree_t *prev = NULL;
+
 	if (tree == NULL)
 		return (0);
 
-	if (_binary_tree_inorder(tree, NULL) < 0)
-		return (0);
-	return (1);
+	return (_binary_tree_inorder(tree, &prev));
 }
